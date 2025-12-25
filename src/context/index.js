d79d3844d8dfea64f10ca68ae37abf08aceddf57
Fresherpb35 <ds5513063@gmail.com>
2025-12-25 17:22:26 +0530

    Delete src directory

--- src/context/index.js
+++
@@ -1,4 +0,0 @@
-// contexts/index.js
-export { AuthProvider, useAuth } from "./AuthContext";
-export { CallProvider, useCall } from "./CallContext";
-export { ChatProvider, useChat } from "./ChatContext";
